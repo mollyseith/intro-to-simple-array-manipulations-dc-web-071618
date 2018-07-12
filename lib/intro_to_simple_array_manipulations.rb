43c17962ea96151536f25e06f05b300f778dcc79
Molly Seither <molly.seither@centre.edu>
2018-07-12 19:44:27 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,10 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
+def using_shift(array, string)
+  array.shift(string)
+end
+
 def pop_with_args(array)
   return array.pop
   return array.pop
