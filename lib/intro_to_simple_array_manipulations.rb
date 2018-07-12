a82d7d6f24750f928abcb018648ccf087f92ae4b
Molly Seither <molly.seither@centre.edu>
2018-07-12 19:54:55 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,10 @@ def using_delete(array, string)
   array.delete(string)
 end
 
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
+
 def using_insert(array, element)
   array.insert(4, element)
 end
